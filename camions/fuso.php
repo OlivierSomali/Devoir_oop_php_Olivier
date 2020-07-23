f9f0bb76401b34794b75987c7de9c781f1f566ac
somali <oliviersomali@gmail.com>
2020-07-23 22:01:44 +0200

    affichage des elements provenant dans la classe parent

--- camions/fuso.php
+++ camions/fuso.php
@@ -18,7 +18,9 @@ class Fuso extends Camions{
     }
     public function afficher_fuso()
     {
-        echo 'et le bagage de :' . $this->getBagage();
+        echo  'le nom de camion est de:'  . $this->getNom() .
+        'sa marque est de :' . $this->getMarque() .
+        'et le type de :' .$this->getType() . 'et le bagage de :' . $this->getBagage();
     }
 
 }
