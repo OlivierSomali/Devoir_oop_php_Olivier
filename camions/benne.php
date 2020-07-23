f4ea01f5b2b7175e29084d2e8e0fb9259c4004eb
somali <oliviersomali@gmail.com>
2020-07-23 21:09:01 +0200

    declaration d'attribut

--- camions/benne.php
+++ camions/benne.php
@@ -3,6 +3,9 @@ namespace Devoir_oop_php_Olivier\camions\Benne;
 use Devoir_oop_php_Olivier\camions\Camions\Camions;
 
 class Benne extends Camions{
+     //attribut
+    private $poids;
+
     
 }
 
