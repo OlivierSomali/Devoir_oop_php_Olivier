fcd976a2b83a8159189f5f702db9f9f8a993c619
somali <oliviersomali@gmail.com>
2020-07-23 19:58:31 +0200

    initialisation des attributs dans le constructeur

--- camions/camions_lourd.php
+++ camions/camions_lourd.php
@@ -9,9 +9,11 @@ class camions{
     private $marque; // les droits d'acces
     private $type;
 
-    public function __construct($nom,$marque,$type)
+    public function __construct($nom,$marque,$type) //le constructeur
     {
-        
+        $this->nom=$nom;
+        $this->marque=$marque; //initialisation des attributs dans le constructeur
+        $this->type=$type;
     }
 }
 
