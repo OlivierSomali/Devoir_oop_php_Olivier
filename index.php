aef0f0c6355e9ab49e337b11991879ccdc3e3800
somali <oliviersomali@gmail.com>
2020-07-23 21:36:25 +0200

    ajout des include

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ use Devoir_oop_php_Olivier\camions\Camions\Camions;
 use Devoir_oop_php_Olivier\camions\Benne\Benne;
 use Devoir_oop_php_Olivier\camions\Fuso\Fuso;
 
-include
+include('camions/camions_lourd.php');
+include('camions/benne.php');
+include('camions/fuso.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
