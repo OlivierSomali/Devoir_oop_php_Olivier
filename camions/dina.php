254a419ec00b74bf2c0940860ae4cc34bcbbc12c
somali <oliviersomali@gmail.com>
2020-07-28 18:13:31 +0200

    ajout de commentaire

--- camions/dina.php
+++ camions/dina.php
@@ -1,22 +1,22 @@
 <?php
 
 namespace Devoir_oop_php_Olivier\camions\Dina;
-use Devoir_oop_php_Olivier\camions\Camions\Camions;
+use Devoir_oop_php_Olivier\camions\Camions\Camions; // la racine
 
-class Dina extends Camions{
+class Dina extends Camions{  // declaration de la classe fille qui herite la classe parent
     //attribut
     private $nbre_pneu=6;
     
-    public function __construct($nom,$marque,$type)
+    public function __construct($nom,$marque,$type) //constructeur
     {
-        parent::__construct($nom,$marque,$type);
+        parent::__construct($nom,$marque,$type);//appel de constructeur
     }
 
-    public function getNbre_pneu()
+    public function getNbre_pneu()// la fonction magique get
     {
         return $this->nbre_pneu;
     }
-    public function afficher_dina()
+    public function afficher_dina() //la fonction d'affichage
     {
         echo '<h2>'.'le nom de camion est de:'  .  $this->getNom()  . 
         '&nbsp'.'sa marque ' .$this->getMarque() .'&nbsp'.'avec le type de:' . $this->getType()    . '&nbsp' . 'et leur nombre de pneu:' .$this->getNbre_pneu() . 'pneus'. '</h2>';
